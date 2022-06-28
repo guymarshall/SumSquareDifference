d7cc9c5fc74f22dc1d11bb5be695e9d40a5f17a3
Guy Marshall <guymarshall9@gmail.com>
2022-06-28 19:18:33 +0100

    Initial commit

---
+++ SumSquareDifference.cpp
@@ -0,0 +1,21 @@
+// The sum of the squares of the first ten natural numbers is
+//     1^2 + 2^2 + ... + 10^2 = 385
+
+// The square of the sum of the first ten natural numbers is
+//     (1 + 2 + ... + 10)^2 = 3025
+
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+//     3025 - 385 = 2640
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+#include <iostream>
+#include "Sum.hpp"
+
+#define LOG(x) std::cout << x << std::endl
+
+int main(int argc, char const *argv[])
+{
+    LOG("Hello, World!");
+    return 0;
+}
